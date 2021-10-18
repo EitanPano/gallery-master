cca1c222133542f578768f964392435c31202537
Eitan Pano <eitanpano95@gmail.com>
2021-10-18 20:32:16 +0300

    added contacting functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-'use strict';
-
+"use strict";
 
 $(onInit);
 
 function onInit() {
-  console.log('Starting up');
-  renderProjs();
+    console.log("Starting up");
+    renderProjs();
 }
 
+function contactUs() {
+    const inputEmail = $('inputEmail').val()
+    const inputSubject = $('#inputSubject').val()
+    const inputFlaotingText = $('#inputFloatingText').val()
+
+    window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=me@example.com&su=${inputSubject}&body=${inputFlaotingText}`);
+}
 
