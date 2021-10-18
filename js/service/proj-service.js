7a204f232c126c3b0c791ca2b218e5272a1b0cc7
Eitan Pano <eitanpano95@gmail.com>
2021-10-18 14:08:15 +0300

    added minesweeper proj url and img

--- js/service/proj-service.js
+++ js/service/proj-service.js
@@ -1,17 +1,34 @@
-'use strict';
+"use strict";
 
 let gProjs;
+_createProjs();
+
+console.log(gProjs);
 
 function _createProj(name, title, desc, url, imgUrl, labels) {
     const proj = {
-        id: makeId(),
+        id: name.toLowerCase(),
         name,
         title,
         desc,
         url,
         imgUrl,
         publishedAt: new Date(),
-        labels
+        labels,
     };
     return proj;
+}
+
+function _createProjs() {
+    const projs = [
+        _createProj(
+            "Minersweeper",
+            "Don't Blow Up",
+            "in progress",
+            "https://eitanpano.github.io/Minesweeper/",
+            "../../img/minesweeper.png",
+            ["Matrixes", "Css"],
+        ),
+    ];
+    gProjs = projs;
 }
